c3928487380e332599e1f7c156269179e046d726
Maxym <m.yalaletdinov@gmail.com>
2023-01-23 13:37:35 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+import tabs from './modules/tabs';
+import calc from './modules/calc';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import modal from './modules/modal';
+import slider from './modules/slider';
+import timer from './modules/timer';
+import { openModal } from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', function() {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+
+          tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+          calc();
+          cards();
+          forms('form', modalTimerId);
+          modal('[data-modal]', '.modal', modalTimerId);
+          slider();
+          timer('.timer', '2023-03-25');
+
+});
